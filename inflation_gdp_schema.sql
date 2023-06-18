e20e360249052eb30bb32cb358cf8273a01e4c43
patelavani332 <120197958+patelavani332@users.noreply.github.com>
2023-06-18 08:20:42 -0400

    Added the sql database schema

---
+++ inflation_gdp_schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE if exists inflation_gdp;
+
+CREATE TABLE inflation_gdp(
+	country VARCHAR (30) not null,
+	country_id integer not null,
+	year bigint not null,
+	Inflation DOUBLE PRECISION not null,
+	GDP DOUBLE PRECISION not null,
+	interestRate DOUBLE PRECISION not null,
+	unemployment DOUBLE PRECISION not null,
+	outcome integer not null
+)
+
+select * from inflation_gdp;
